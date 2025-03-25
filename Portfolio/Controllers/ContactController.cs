d77fd85cad7d5587a2ac9e23b94f6eb1eb4a6869
63915 <63915@KIT>
2025-03-25 12:28:32 +0800

    Add project files.

---
+++ Portfolio/Controllers/ContactController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class ContactController : Controller
+{
+    public IActionResult Contact()
+    {
+        return View();
+    }
+}
+
---
+++ Portfolio/Controllers/PortfolioController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+public class PortfolioController : Controller
+{
+    public IActionResult Portfolio()
+    {
+        var projects = new List<Project>
+        {
+            new Project { Id = 1, Title = "Project One", Description = "A cool project.", ImageUrl = "https://via.placeholder.com/400" },
+            new Project { Id = 2, Title = "Project Two", Description = "Another amazing project.", ImageUrl = "https://via.placeholder.com/400" },
+            new Project { Id = 3, Title = "Project Three", Description = "Yet another great project.", ImageUrl = "https://via.placeholder.com/400" }
+        };
+
+        return View(projects);
+    }
+}
+
